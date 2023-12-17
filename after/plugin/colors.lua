ecd9615b8ae65bf9b565781e8094a5bbbeae91fd
Quiomachado <quiomachado@gmail.com>
2023-12-17 01:14:12 +0000

    done

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -20,10 +20,14 @@ require('onedarkpro').setup({
 
 require('tokyonight').setup({
     transparent = true,
+    styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+    }
 })
 
 function ColorMyPencils(color)
-    color = color or "gruvbox"
+    color = color or "tokyonight"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,7 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -38,7 +38,7 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(_, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -104,4 +104,3 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
-
