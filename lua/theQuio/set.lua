daec4a919c706fe32d4cb1b7128edb8ceb384c18
Henriquevmac <quiomachado@gmail.com>
2024-09-29 20:32:58 +0100

    added support for hyprls

--- lua/theQuio/set.lua
+++ lua/theQuio/set.lua
@@ -7,6 +7,18 @@ vim.api.nvim_exec([[
   augroup END
 ]], false)
 
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+		pattern = {"*.hl", "hypr*.conf"},
+		callback = function(event)
+				print(string.format("starting hyprls for %s", vim.inspect(event)))
+				vim.lsp.start {
+						name = "hyprlang",
+						cmd = {"hyprls"},
+						root_dir = vim.fn.getcwd(),
+				}
+		end
+})
+
 o.number = true
 o.relativenumber = true
 
