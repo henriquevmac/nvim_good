818ab9914f75911bb1494c576af56078c7829c65
Henriquevmac <quiomachado@gmail.com>
2024-12-06 22:30:53 +0000

    up tele

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,13 +8,14 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        --vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+        -- vim.keymap.set('n', '<leader>ps', function()
+        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        -- end)
         vim.keymap.set('n', '<leader>pb', builtin.treesitter, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>pm', builtin.man_pages, {})
         vim.keymap.set('n', '<leader>vs', builtin.lsp_document_symbols, {})
+        vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
     end
 }
