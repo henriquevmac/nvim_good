863a2da0b40c2f4cf94f7b2cb1154f0916481515
Henriquevmac <quiomachado@gmail.com>
2024-12-22 00:21:02 +0000

    update colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "gruvbox"
+    color = color or "tokyonight-moon"
     vim.cmd.colorscheme(color)
 end
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,17 @@ return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
-        'nvim-lua/plenary.nvim'
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
+        require('telescope').setup {
+            extensions = {
+                fzf = {}
+            }
+        }
+        require('telescope').load_extension('fzf')
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
@@ -17,5 +25,6 @@ return {
         vim.keymap.set('n', '<leader>pm', builtin.man_pages, {})
         vim.keymap.set('n', '<leader>vs', builtin.lsp_document_symbols, {})
         vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+        vim.keymap.set('n', '<leader>ec', builtin.colorscheme, {})
     end
 }
